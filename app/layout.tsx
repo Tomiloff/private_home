35899578fd0ca6b9a92ecd7085dda179bb804b89
Vadim <Tomilov_Vadim@list.ru>
2023-09-12 23:04:10 +0500

    Test commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Private Home',
+  title: 'Main version',
   description: 'Test task',
 }
 
