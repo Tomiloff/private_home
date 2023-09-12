8270e923b65ab5ed296c9a939d8d006bfc528b43
Vadim <Tomilov_Vadim@list.ru>
2023-09-12 22:53:47 +0500

    Initialization app

---
+++ app/page.tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <div>
+        </div>
+      </div>
+
+      <div className={styles.center}>
+        <h1>Private Home</h1>
+      </div>
+
+      <div className={styles.grid}>
+        <a
+          href="#"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>-&gt;</span>
+          </h2>
+        </a>
+
+        <a
+          href="#"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>-&gt;</span>
+          </h2>
+        </a>
+
+        <a
+          href="#"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>-&gt;</span>
+          </h2>
+        </a>
+
+        <a
+          href="#"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>-&gt;</span>
+          </h2>
+        </a>
+      </div>
+    </main>
+  )
+}
