26df8a6f4d174cee5c6229725306477d1c800eee
Vadim <Tomilov_Vadim@list.ru>
2023-09-14 00:15:51 +0500

    Configuring global css and fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,14 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <div>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <h1>Main version</h1>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="#"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-        </a>
-
-        <a
-          href="#"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-        </a>
-
-        <a
-          href="#"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-        </a>
-
-        <a
-          href="#"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-        </a>
-      </div>
+    <main>
+      <h1>Inchapin</h1>
+      <h2>Заказать звонок</h2>
+      <h3>INCHAPIN</h3>
+      <h4>INCHAPIN</h4>
+      <p>test</p>
+      <span>test</span>
     </main>
   )
 }
