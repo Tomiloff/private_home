d215ee69e7758fc3dded6414e4a17ec4763b991d
Vadim <Tomilov_Vadim@list.ru>
2023-10-01 00:31:54 +0500

    Changed text in btn feedback

--- components/layout/Header/HeaderCallbackPanel/HeaderCallbackPanel.tsx
+++ components/layout/Header/HeaderCallbackPanel/HeaderCallbackPanel.tsx
@@ -4,7 +4,7 @@ import { CallbackPanel } from "@/components/common/CallbackPanel/CallbackPanel";
 export const HeaderCallbackPanel: FC = () => {
   return (
     <CallbackPanel
-      text="Заказать обратный звонок"
+      text="Заказать звонок"
       phoneNumber="+7 495 527 21 21"
     />
   )
